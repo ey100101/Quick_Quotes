08ab971a138ee25c924a049f88f2439888683a41
Emily <ey100101@gmail.com>
2015-07-17 13:02:54 +0000

    updates day 2

--- models/model.rb
+++ models/model.rb
@@ -9,4 +9,14 @@ class Quotes
   def author(quote)
     return @quotes[quote][0]
   end
+  
+  def tag(quote)
+    array = []
+    length = @quotes[quote].length
+    while length > 0
+      array.push(@quotes[quote][length])
+      length -= 1
+    end
+    return array
+  end
 end
